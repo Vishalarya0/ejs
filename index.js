1b586a064f4c6d59f68b87968a22d5754a6b2585
Kumarvishal22665@ <kumarvishal22665@icloud.com>
2025-09-06 09:41:20 +0530

    set path for ejs

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const port = 3000;
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
+app.set("views",pathjoin(__dirname,"/views"));
+
 
 app.get('/', (req, res) => {
     res.render("home.ejs");
 });
 
+app.get('/about', (req, res) => {
+    res.send("hello about page");
+});
 
 app.listen(port, () => {
   console.log(`EJS app listening at http://localhost:${port}`);

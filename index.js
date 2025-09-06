4cb977ed6129c15e1ce7a3afc6d467125ed2a3fe
Kumarvishal22665@ <kumarvishal22665@icloud.com>
2025-09-06 09:48:26 +0530

    join path

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
 const port = 3000;
 
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
-app.set("views",pathjoin(__dirname,"/views"));
+app.set("views",path.join(__dirname,"/views"));
 
 
 app.get('/', (req, res) => {

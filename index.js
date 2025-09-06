f32b48a6a4ca99aa49f775922fa18de74ad5ffb6
Kumarvishal22665@ <kumarvishal22665@icloud.com>
2025-09-06 09:27:11 +0530

    set ejs

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.listen(port, () => {
+  console.log(`EJS app listening at http://localhost:${port}`);
+});
